a9bd57702a490c55d649360228e022b2a994cc0c
ronferens <ronferens@gmail.com>
2023-07-29 08:31:14 +0300

    Stable implementation of TransPoseNet

--- models/transposenet/TransPoseNet.py
+++ models/transposenet/TransPoseNet.py
@@ -13,50 +13,52 @@ class TransPoseNet(BasePoseLightningModule):
     def __init__(self, config):
         super(TransPoseNet, self).__init__(config)
 
-        # Efficient net
-        self.backbone_dim = 1280
-        self.latent_dim = 1024
+        self.config = config
 
-        # Position (t) and orientation (rot) encoders
+        # The projection of the activation map before going into the Transformer's encoder
+        self.trans_in_encoder = nn.Conv2d(112, 256, kernel_size=1)
+        self.orient_encoder = nn.Conv2d(40, 256, kernel_size=1)
+
+        # Translation and orientation encoders
         self.decoder_dim = config.get('decoder_dim')
         self.trans_vit = SimpleViT(image_size=14,
-                                   patch_size=2,
-                                   num_classes=1024,
-                                   dim=256,
-                                   depth=6,
-                                   heads=4,
-                                   channels=112,
-                                   mlp_dim=1024
+                                   patch_size=1,
+                                   num_classes=self.config.get('latent_dim'),
+                                   dim=self.config.get('decoder_dim'),
+                                   depth=self.config.get('depth'),
+                                   heads=self.config.get('heads'),
+                                   channels=self.config.get('decoder_dim'),
+                                   mlp_dim=self.config.get('decoder_dim')
                                    )
         self.orient_vit = SimpleViT(image_size=28,
-                                    patch_size=2,
-                                    num_classes=1024,
-                                    dim=256,
-                                    depth=6,
-                                    heads=4,
-                                    channels=40,
-                                    mlp_dim=1024
+                                    patch_size=1,
+                                    num_classes=self.config.get('latent_dim'),
+                                    dim=self.config.get('decoder_dim'),
+                                    depth=self.config.get('depth'),
+                                    heads=self.config.get('heads'),
+                                    channels=self.config.get('decoder_dim'),
+                                    mlp_dim=self.config.get('decoder_dim')
                                     )
 
         # Regressor layers
-        # self.trans_fc_in = nn.Linear(256, self.latent_dim)
-        self.trans_fc_out = nn.Linear(self.latent_dim, 3)
-        # self.orient_fc_in = nn.Linear(256, self.latent_dim)
-        self.orient_fc_out = nn.Linear(self.latent_dim, 4)
-
-        self.trans_dropout = nn.Dropout(p=0.1)
-        self.orient_dropout = nn.Dropout(p=0.1)
+        self.trans_fc = nn.Linear(self.config.get('latent_dim'), 3)
+        self.orient_fc = nn.Linear(self.config.get('latent_dim'), 4)
 
     def forward(self, x):
+        # Extracting feature maps from the Backbone's forward pass
         features = self._backbone.extract_endpoints(x)
         src_t = features['reduction_4']
-        src_rot = features['reduction_3']
+        src_orient = features['reduction_3']
 
-        x_trans = self.trans_dropout(F.relu(self.trans_vit(src_t)))
-        x_orient = self.orient_dropout(F.relu(self.orient_vit(src_rot)))
+        # Translation branch forward pass
+        trans_input = self.trans_in_encoder(src_t)
+        x_trans = F.gelu(self.trans_vit(trans_input))
+        p_x = self.trans_fc(x_trans)
 
-        p_x = self.trans_fc_out(x_trans)
-        p_q = self.orient_fc_out(x_orient)
+        # Orientation branch forward pass
+        orient_input = self.orient_encoder(src_orient)
+        x_orient = F.gelu(self.orient_vit(orient_input))
+        p_q = self.orient_fc(x_orient)
 
         return torch.cat((p_x, p_q), dim=1)
 
